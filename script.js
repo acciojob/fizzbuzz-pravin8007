212c6ebf1b48569ce53e02e921cc8222a5130315
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-11-11 20:47:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+// Function to generate FizzBuzz output for numbers from 1 to 20
+function fizzBuzz() {
+  let output = '';
+
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      output += 'FizzBuzz\n';
+    } else if (i % 3 === 0) {
+      output += 'Fizz\n';
+    } else if (i % 5 === 0) {
+      output += 'Buzz\n';
+    } else {
+      output += i + '\n';
+    }
+  }
+
+  return output;
+}
+
+// Display the result using alert
+alert(fizzBuzz());
+
